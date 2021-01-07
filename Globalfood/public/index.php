999ab3de3a22a5162a35b6bfef407a0592169b96
vishnu-spec <76815457+vishnu-spec@users.noreply.github.com>
2021-01-07 21:20:16 +0530

    Add files via upload
    PHP web page 
     -edit page 
     -create page
     -delete page

---
+++ Globalfood/public/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title> Global food </title>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+
+      <h1> Global food : door delivery </h1>
+  </body>
+</html>
